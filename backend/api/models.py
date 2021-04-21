e54390da28ae2fda2adec2b72607fc4a1eba3e01
Ilya Mikhailovsky <perfoys.officialelite@gmail.com>
2021-04-21 16:57:21 +0300

    Created model and made migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Log.objects.filter(code=code).count() == 0:
+            break
+    
+    return code
+
 
 # Create your models here.
+class Log(models.Model):
+    session = models.CharField(max_length=8, default="", unique=True)
+    author = models.CharField(max_length=50, unique=True)
+    text = models.CharField(max_length=256)
+    audio_num = models.CharField(max_length=256)
+    date = models.DateTimeField(auto_now_add=True)
