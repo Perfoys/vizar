1072aa4853edf1c94dc63ebd6238015ddc49d305
Ilya Mikhailovsky <perfoys.officialelite@gmail.com>
2021-04-22 02:09:54 +0300

    Added simple view

---
+++ backend/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Log
+
+class LogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Log
+        fields = ('id', 'session', 'author', 'text', 'audio_num', 'date')
+        
